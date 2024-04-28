74dbbf324956e29e139af58cb843bb7aaea1d908
wz156700 <wz156700@noreply.gitcode.com>
2024-04-28 19:09:55 +0800

    更新

--- circle-effect.glsl
+++ circle-effect.glsl
@@ -1,11 +1,42 @@
+// 模糊效果
+// void mainImage(out vec4 fragColor,in vec2 fragCoord){
+//     vec2 uv =fragCoord/iResolution.xy; //坐标除以画布大小得到归一化uv坐标
+//     // uv 居中
+//     uv=(uv*2.)-1.0;
+//     uv.x*=iResolution.x/iResolution.y; // 修正uv坐标未自动适应画布比例的问题
+//     float d=(length(uv)-0.5); // 取一段距离
+//     float c=0.; //存放值的变量
+//     //模糊效果
+//     c=smoothstep(0.,0.2,d ); //利用step函数得到值
+//     fragColor=vec4(vec3(c),1.); // 输出
+    
+// }
+
+
+// 白色圆
+// void mainImage(out vec4 fragColor,in vec2 fragCoord){
+//     vec2 uv =fragCoord/iResolution.xy; //坐标除以画布大小得到归一化uv坐标
+//     // uv 居中
+//     uv=(uv*2.)-1.0;
+//     uv.x*=iResolution.x/iResolution.y; // 修正uv坐标未自动适应画布比例的问题
+//     float d=-1./(length(uv)-0.5); // 取一段距离
+//     float c=0.; //存放值的变量
+//     //模糊效果
+//     c=smoothstep(0.,0.5,d ); //利用step函数得到值
+//     // 发光效果
+//     fragColor=vec4(vec3(c),1.); // 输出
+// }
+
+
+//发光效果
 void mainImage(out vec4 fragColor,in vec2 fragCoord){
     vec2 uv =fragCoord/iResolution.xy; //坐标除以画布大小得到归一化uv坐标
     // uv 居中
     uv=(uv*2.)-1.0;
     uv.x*=iResolution.x/iResolution.y; // 修正uv坐标未自动适应画布比例的问题
-    float d=length(uv)-0.5; // 取一段距离
-    float c=0.; //存放值的变量
-    //模糊效果
-    c=smoothstep(0.,0.2,d ); //利用step函数得到值
-    fragColor=vec4(c,c,c,1.); // 输出
+    float d=(length(uv)); // 取uv坐标长度（这里不需要减去一段距离，因为减去一段距离之后便存在负数了）
+    float c=0.2/d; //存放值的变量
+    // 发光效果
+    fragColor=vec4(vec3(c),1.); // 输出
+    
 }
--- circle.glsl
+++ circle.glsl
@@ -13,19 +13,18 @@
 //     fragColor=vec4(c,c,c,1.); // 输出
 // }
 
+float sdCircle(vec2 p, float r) {
+    return length(p) - r;
+}
+
 void mainImage(out vec4 fragColor,in vec2 fragCoord){
     vec2 uv =fragCoord/iResolution.xy; //坐标除以画布大小得到归一化uv坐标
-    // fragColor=vec4(uv.x,0,0.,1.0);
-    //  fragColor=vec4(0.,uv.y,0.,1.0);
-    // fragColor=vec4(uv.x,uv.y,0.,1.0);
-    // float d=length(uv);
-    // fragColor=vec4(vec3(d),1.);
     // uv 居中
     uv=(uv*2.)-1.0;
-    // float d=length(uv);
-    // fragColor=vec4(vec3(d),1.);
     uv.x*=iResolution.x/iResolution.y; // 修正uv坐标未自动适应画布比例的问题
-    float d=length(uv)-0.5; // 取一段距离
+    //应用SDF函数的圆形
+    float d=sdCircle(uv,0.5); // 取一段距离
+    
     float c=0.; //存放值的变量
     c=smoothstep(0.,0.02,d ); //利用step函数得到值
     fragColor=vec4(c,c,c,1.); // 输出
